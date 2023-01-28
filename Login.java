eef81003e3deaf32e4573ebaf5db5d9d824a9cc8
Yogesh Aswar <yogeshaswar46@gmail.com>
2023-01-28 16:57:32 +0530

    structure completed

--- Login.java
+++ Login.java
@@ -5,7 +5,7 @@ public class Login extends JFrame {
     Login() {
         setLayout(null);
         // setSize defines dimensions
-        setSize(800, 500);
+        setSize(1000, 500);
         // setVisible makes visible
         setVisible(true);
         // Screen Location
@@ -32,21 +32,40 @@ public class Login extends JFrame {
         add(heading);
         // Card
         JLabel card = new JLabel("Card No");
-        card.setBounds(120, 150, 150, 40);
+        card.setBounds(120, 150, 150, 30);
         card.setFont(new Font("Raleway", Font.BOLD, 28));
         add(card);
-        // Pin
-        JLabel pin = new JLabel("Pin");
-        pin.setBounds(120, 220, 150, 40);
-        pin.setFont(new Font("Raleway", Font.BOLD, 28));
-        add(pin);
-
         // Input card
         JTextField cardInput = new JTextField();
-        cardInput.setBounds(300, 150, 250, 40);
+        cardInput.setBounds(300, 150, 250, 30);
+        // Pin
+        JLabel pin = new JLabel("Pin");
+        pin.setBounds(120, 220, 150, 30);
+        pin.setFont(new Font("Raleway", Font.BOLD, 28));
+        add(pin);
         // Input pin
         JTextField pinInput = new JTextField();
-        pinInput.setBounds(300, 220, 250, 40);
+        pinInput.setBounds(300, 220, 250, 30);
+
+        // Button
+
+        JButton login = new JButton("SIGN IN");
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.setBounds(300, 300, 100, 30);
+        add(login);
+
+        JButton clear = new JButton("CLEAR");
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        clear.setBounds(430, 300, 100, 30);
+        add(clear);
+        JButton signUp = new JButton("SIGN UP");
+        signUp.setBackground(Color.BLACK);
+        signUp.setForeground(Color.WHITE);
+        signUp.setBounds(400, 350, 100, 30);
+        add(signUp);
+
     }
 
     public static void main(String[] args) {
