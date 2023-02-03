1dc94112bd7fb90260638151b224d23182a40ec2
Yogesh Aswar <yogeshaswar46@gmail.com>
2023-02-03 13:09:02 +0530

    Added change pin functionality

---
+++ PinChange.java
@@ -0,0 +1,21 @@
+import java.awt.Color;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+// import java.sql.Date; //date
+// import java.util.*;
+import java.util.Date; //date
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import com.mysql.cj.Query;
+
+public class PinChange {
+    String pinNumber;
+    JTextField widrawAmtInput;
+    JButton widraw, back;
+    public PinChange(String pinNumber) {
+        this.pinNumber = pinNumber;
+        
+    }
+}
